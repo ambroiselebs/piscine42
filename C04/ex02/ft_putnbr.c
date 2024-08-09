e43960c9bce94effe64edde895bba576972a5cd8
ambroiselebs <amberenger06@gmail.com>
2024-08-09 19:50:11 +0200

    C04 ex02

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberenge <marvin@42.fr>                    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-08-09 16:56:18 by aberenge          #+#    #+#             */
+/*   Updated: 2024-08-09 16:56:18 by aberenge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_number(char *str, int size)
+{
+	while (size > 0)
+	{
+		size--;
+		write(1, &str[size], 1);
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	char	final[12];
+	int		i;
+
+	if (nb == 0)
+		write(1, "0", 1);
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+	i = 0;
+	while (nb > 0)
+	{
+		final[i] = (nb % 10) + '0';
+		nb = nb / 10;
+		i++;
+	}
+	print_number(final, i);
+}
+/*
+int	main(void)
+{
+	ft_putnbr(-42);
+
+	return (0);
+}*/
