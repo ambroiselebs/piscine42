3e6d4c62656f1e20ea2b095ad3693a30f4b3e855
ambroiselebs <amberenger06@gmail.com>
2024-08-10 14:33:09 +0200

    Correction ft_atoi (norne) + C05 ex01

---
+++ C05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial,c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberenge <marvin@42.fr>                    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-08-10 12:24:17 by aberenge          #+#    #+#             */
+/*   Updated: 2024-08-10 12:24:17 by aberenge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb <= 1)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
+
+/* int main(void)
+{
+	int res = ft_recursive_factorial(5);
+	printf("%d", res);
+
+	return (0);
+} */
