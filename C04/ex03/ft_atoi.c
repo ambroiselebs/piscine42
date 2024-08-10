95b905c2fa64dee5beb62612b2982322914b5c0c
ambroiselebs <amberenger06@gmail.com>
2024-08-10 11:59:31 +0200

    ft_atoi debut

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberenge <marvin@42.fr>                    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-08-10 09:38:48 by aberenge          #+#    #+#             */
+/*   Updated: 2024-08-10 09:38:48 by aberenge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	parse_str(char *str, char *new)
+{
+	int	i;
+	int	j;
+	int	res;
+
+	i = 0;
+	j = 0;
+	res = 0;
+	while (str[i])
+	{
+		if (str[i] == '+')
+			res++;
+		if (str[i] == '-')
+			res--;
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			new[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	opp;
+	int	res;
+	char	new[ft_strlen(str)];
+
+	opp = parse_str(str, new);
+	i = 0;
+	while (new[i])
+	{
+		res = res * 10 + (new[i] - '0');
+		i++;
+	}
+	if (opp < 0)
+		res = -res;
+	return (res);
+}
+
+int main(void)
+{
+	int res = ft_atoi(" ---+--+1234ab567");
+	printf("%d", res);
+
+	return (0);
+}
