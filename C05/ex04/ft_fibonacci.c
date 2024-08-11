009efa14ec807003609d70927d263e8017881a6c
ambroiselebs <amberenger06@gmail.com>
2024-08-11 11:18:08 +0200

    C05 + C06

---
+++ C05/ex04/ft_fibonacci.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberenge <marvin@42.fr>                    #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-08-10 15:44:27 by aberenge          #+#    #+#             */
+/*   Updated: 2024-08-10 15:44:27 by aberenge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_fibonacci(int index)
+{
+	if (index == 1)
+		return (1);
+	if (index == 0)
+		return (0);
+	if (index < 0)
+		return (-1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+/* int main(void)
+{
+	printf("%d", ft_fibonacci(11));
+	return (0);
+} */
