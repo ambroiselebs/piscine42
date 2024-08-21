2e9b6b2ac2850e47365262354c095c86ce489760
Ambroise Berenger <aberenge@c1r5p12.42lehavre.fr>
2024-08-21 11:26:59 +0200

    sort ntm

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberenge <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 11:11:15 by aberenge          #+#    #+#             */
+/*   Updated: 2024/08/21 11:25:15 by aberenge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int	main(int argc, char **argv)
+{
+	char	temp;
+	int		i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+			ft_swap(&argv[i], &argv[i + 1]);
+		i++;
+	}
+}
